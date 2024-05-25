662ba1bd511d23cfe75382e78d15479b0a483cfc
levandat14 <levandatfake1@gmail.com>
2024-05-25 19:58:10 +0700

    demo project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Kiểm tra và bật hiển thị lỗi (chỉ trong quá trình phát triển, tắt khi đưa vào sản xuất)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Kiểm tra không có đầu ra trước lệnh header
+if (!headers_sent()) {
+    header("Location: TrangChu/Dichvu.php");
+    exit;
+} else {
+    echo "Cannot redirect, headers already sent!";
+}
+?>
