be78e23609f6f782aa8ef732a5f8cf61459a66bb
levandat14 <levandatfake1@gmail.com>
2024-06-19 17:35:59 +0700

    demo project

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-// Kiểm tra và bật hiển thị lỗi (chỉ trong quá trình phát triển, tắt khi đưa vào sản xuất)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Kiểm tra không có đầu ra trước lệnh header
-if (!headers_sent()) {
-    header("Location: TrangChu/ChucNang_Dangnhap/Dangnhap.php");
-    exit;
-} else {
-    echo "Cannot redirect, headers already sent!";
-}
+    header("Location: TrangChu/index.php");
 ?>
