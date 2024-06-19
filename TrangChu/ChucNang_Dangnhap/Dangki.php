1a9187acd740057086e1e288cd0499ea3c6b50c2
levandat14 <levandatfake1@gmail.com>
2024-06-19 17:34:10 +0700

    demo project

--- TrangChu/ChucNang_Dangnhap/Dangki.php
+++ TrangChu/ChucNang_Dangnhap/Dangki.php
@@ -7,24 +7,7 @@
 
   <link href="css/style.css" rel="stylesheet" />
   <?php
-
-        $dbHost = 'roundhouse.proxy.rlwy.net';
-        $dbUser = 'root';
-        $dbPassword = 'BjDQDkrAbzTfSyfYLhqNeyljauKFdVyn';
-        $dbName="railway";
-        $dbPort = 48011;
-
-        $conn = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName,$dbPort);
-        if($conn)
-        {
-            $setLang=mysqli_query($conn, "SET NAMES 'utf8'");
-        }
-        else{
-            die("ket noi that bai".mysqli_connect_errno());
-        }
-
-    ?>
-  <?php
+    require_once("../ketnoi/ketnoi.php");
     $error_xacnhanmatkhau = null;
     $error_taikhoan =null;
     if (isset($_POST['btnsubmit'])) {
@@ -161,4 +144,3 @@
           <script src="js/main.js"></script>
       </body>
   </form>
-  <script src='https://www.google.com/recaptcha/api.js'></script>
